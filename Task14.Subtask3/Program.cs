5b7b3d6b91e14653220f3b1c3cda5be38a2ef7ca
Ekrador <101429944+Ekrador@users.noreply.github.com>
2023-03-05 18:14:39 +0500

    Добавьте файлы проекта.

---
+++ Task14.Subtask3/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task14.Subtask3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  создаём пустой список с типом данных Contact
+            var phoneBook = new List<Contact>();
+
+            // добавляем контакты
+            phoneBook.Add(new Contact("Игорь", "Николаев", 79990000001, "igor@example.com"));
+            phoneBook.Add(new Contact("Сергей", "Довлатов", 79990000010, "serge@example.com"));
+            phoneBook.Add(new Contact("Анатолий", "Карпов", 79990000011, "anatoly@example.com"));
+            phoneBook.Add(new Contact("Валерий", "Леонтьев", 79990000012, "valera@example.com"));
+            phoneBook.Add(new Contact("Сергей", "Брин", 799900000013, "serg@example.com"));
+            phoneBook.Add(new Contact("Иннокентий", "Смоктуновский", 799900000013, "innokentii@example.com"));
+
+            SortContacts(phoneBook);
+        }
+
+        public static void SortContacts(List<Contact> phonebook)
+        {
+            var phoneBook = phonebook.OrderBy(x => x.Name).ThenBy(x => x.LastName);
+            int maxPageNumber = phonebook.Count() / 2;
+            while (true)
+            {
+                Console.WriteLine("Введите интересующий номер страницы с контактами...");
+
+                var input = Console.ReadKey().KeyChar;
+
+                var parsed = Int32.TryParse(input.ToString(), out int pageNumber);
+
+                if (!parsed || pageNumber < 1 || pageNumber > maxPageNumber)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Страницы не существует");
+                    Console.WriteLine();
+                }
+                else
+                {
+                    var pageContent = phoneBook.Skip((pageNumber - 1) * 2).Take(2);
+                    Console.WriteLine();
+
+                    foreach (var entry in pageContent)
+                        Console.WriteLine(entry.Name + " " + entry.LastName + ": " + entry.PhoneNumber);
+
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+    public class Contact // модель класса
+    {
+        public Contact(string name, string lastName, long phoneNumber, String email) // метод-конструктор
+        {
+            Name = name;
+            LastName = lastName;
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
+
+        public String Name { get; }
+        public String LastName { get; }
+        public long PhoneNumber { get; }
+        public String Email { get; }
+    }
+}
